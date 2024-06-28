6964a83857287b839994e71ee6ffa33c7a091a7d
h4de5ing <lex109@qq.com>
2024-06-28 12:41:43 +0800

    引入scrcpy源码

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
 }
 gradle.projectsEvaluated {
     tasks.withType<JavaCompile>().configureEach {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,5 @@ dependencyResolutionManagement {
 rootProject.name = "WIFIAP"
 include(":app")
 include(":usbp2p")
+include(":screen")
+include(":scrcpy")
