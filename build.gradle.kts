882c6b68f8558ab03562b650c8bcd1191f402bd6
h4de5ing <lex109@qq.com>
2025-06-30 17:14:16 +0800

    更新项目构建配置，升级Kotlin版本至2.2.0，同步更新Kotlin Android插件和Compose插件版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.11.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
 }
 gradle.projectsEvaluated {
     tasks.withType<JavaCompile>().configureEach {
